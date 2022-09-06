99963fc9bedb667e3652cc2ed2ad6d3b6f6d4017
Davy <davyl@outlook.fr>
2022-09-06 15:12:04 +0200

    QCM2

--- qcm.php
+++ qcm.php
@@ -1,37 +1,80 @@
 <?php
 
-class Qcm {
+class Qcm
+{
     private array $questions = array();
 
-    public function ajouterQuestions(Question $question){
+    public function ajouterQuestions(Question $question)
+    {
         $this->questions[] = $question;
     }
 
-    public function setAppreciation(){
-
+    public function setAppreciation()
+    {
     }
 
-    public function generer(){
-
-    }
-}
-
-class Question {
-    public function ajouterReponse(){
-
-    }
-
-    public function setExpliquations(){
-
+    public function generer()
+    {
     }
 }
 
-class Reponse {
+class Question
+{
+    protected $question;
+    protected array $reponses = [];
+
+    public function construct($question)
+    {
+        $this->question = $question;
+    }
+
+    public function ajouterReponse(Reponse $reponse)
+    {
+        $this->reponses[] = $reponse;
+    }
+
+
+    public function setExpliquations()
+    {
+    }
+}
+
+class Reponse
+{
     const BONNE_REPONSE = true;
     const MAUVAISE_REPONSE = false;
+    private string $reponse;
+    private bool $statut;
 
-    
+
+    public function construct($reponse, $statut = self::MAUVAISE_REPONSE)
+    {
+        $this->reponse = $reponse;
+        $this->statut = $statut;
+    }
+
+    public function getReponse()
+    {
+        return $this->reponse;
+    }
+    public function getStatut()
+    {
+        return $this->statut;
+    }
 }
+
+$qcm = new Qcm();
+
+$question1 = new Question('Et paf, ça fait ...');
+$question1->ajouterReponse(new Reponse('Des mielpops'));
+$question1->ajouterReponse(new Reponse('Des chocapics', Reponse::BONNE_REPONSE));
+$question1->ajouterReponse(new Reponse('Des actimels'));
+$qcm->ajouterQuestions($question1);
+$qcm->generer();
+
+echo '<pre>';
+print_r($qcm);
+echo '</pre>';
 
 ?>
 
